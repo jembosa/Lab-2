1404a64e06952b78fdfbb73e7400bb0a7c90c5eb
Alex Young <alexanderjyoung15@gmail.com>
2023-10-03 21:41:56 -0400

    v2 w loop

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -7,62 +7,67 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            var answer = 0;
+            string stoploop = "";
+            int answer = 0;
+
             Console.WriteLine("Hello, what is your name?");
 
             string name = Console.ReadLine();
 
-            Console.WriteLine($"Hello {name}, please enter a number between 1 and 100 ");
-            answer = int.Parse(Console.ReadLine());
-            while (true)
+            
+            while (stoploop == "")
             {
-                if (answer <=0 )
-                    Console.WriteLine("It clearly says between 1 and 100 dude. Press any any and get out of here.");
-                                if (answer > 100)
+                Console.WriteLine($"Hello {name}, please enter a number between 1 and 100 ");
+
+                while (true)
                 {
+                    answer = int.Parse(Console.ReadLine());
+
+                    if (answer <= 0 || answer > 100)
                     {
-                        Console.WriteLine("It clearly says between 1 and 100 dude. Press any any and get out of here.");
-                            
-                        Console.ReadKey();
-                        return;
+                        Console.WriteLine($"{name}, it clearly says between 1 and 100 dude. Please enter a number between 1 and 100.");
+                    }
+                    else
+                    {                        
+                        break;
                     }
                 }
+                //odd
+                if (answer % 2 != 0)
+
+                    if (answer < 60)
+                    {
+                        Console.WriteLine($"{name}, your number is {answer}, odd, and less than 60");
+                    }
+                    else if (answer > 60)
+                    {
+                        Console.WriteLine($"{name}, your number is {answer}, odd, and greater than 60");
+                    }
+                //even
+                if (answer % 2 == 0)
+
+                    if (answer < 24 && answer > 0)
+                    {
+                        Console.WriteLine($"{name}, your number is {answer}, even, and less than 25");
+                    }
+                    else if (answer < 60 && answer > 0)
+                    { Console.WriteLine($"{name}, your number is {answer}, even, and between 24 and 60 inclusively"); }
+
+                    else if (answer > 60)
+                    {
+                        Console.WriteLine($"{name}, your number is {answer}, even, and greater than 60");
+
+                    }
+
+                //ask to stop here
+                Console.WriteLine("Enter 'stop' to exit.");
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "stop")
                 {
-                   break;
+                    break;
                 }
-            }
-            //odd
-            if (answer % 2 != 0)
-
-                if (answer < 60)
-                {
-                    Console.WriteLine($"{name}, your number is {answer}, odd, and less than 60");
-                }
-            else if (answer > 60)
-            {
-                Console.WriteLine($"{name}, your number is {answer}, odd, and greater than 60");
-            }
-            //even
-            if (answer % 2 == 0)
-
-                if (answer < 24 && answer > 0)
-                {
-                    Console.WriteLine($"{name}, your number is {answer}, even, and less than 25");
-                }
-                else if (answer < 60 && answer > 0)
-                { Console.WriteLine($"{name}, your number is {answer}, even, and between 24 and 60 inclusively"); }
-
-                else if (answer >60)
-                {
-                    Console.WriteLine($"{name}, your number is {answer}, even, and greater than 60");
-
-                }
-
+            }//end stop loop
         }
     }
 }
-
-
-
-
 
